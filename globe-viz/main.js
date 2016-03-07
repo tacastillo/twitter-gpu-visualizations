2cea5b393bcd7672aa677b576629523a066680f3
William Chiang <williamc8899@gmail.com>
2016-03-06 21:21:31 -0800

    Committed dynamic version of the globe
    CURRENT BUG:
    
    The visualization slows down further and further as we switch between
    datasets. This leads me to believe that there is something Three is
    doing in the background in regards to how the canvas is being cleared
    up and it’s causing it to keep the objects in the backend. Will fix
    ASAP.

--- globe-viz/main.js
+++ globe-viz/main.js
@@ -1,48 +1,105 @@
-if (!Detector.webgl) {
+if(!Detector.webgl){
     Detector.addGetWebGLMessage();
 } else {
     var container = document.getElementById('container');
     var globe = new DAT.Globe(container);
+    var currentLang = 'en';
+    var currentRes = 1;
 
-    var setTime = function(globe, t) {
-        return function() {
-            new TWEEN.Tween(globe).to({time: t},500).easing(TWEEN.Easing.Cubic.EaseOut).start();
-        };
-    };
+  var datasetRandomized = [];
+  var currentDate = 0;
 
-    var xhr;
-    TWEEN.start();
-
-    // function createSpan() { 
-    // var spanTag = document.createElement("span"); 
-
-    // spanTag.id = "span1"; 
-
-    // spanTag.className = "dynamicSpan"; 
-
-    // spanTag.innerHTML = "<b>HTML Span tag</b> " 
-    //                     + "created by using "  
-    //                     + "Javascript DOM dynamically."; 
-
-    // document.getElementById("currentInfo").appendChild(spanTag); 
-    // } 
-
+  /* - INITDATA - 
+   * This method is used to manually load all of the JSON data on the client side and parse it into
+   * a randomized series of arrays to simulate the data points being spread across different "dates".
+   * Dynamically generates the DOM elements for browsing through each date.
+   */
+  var initData = function() {
     xhr = new XMLHttpRequest();
     xhr.open('GET', 'data/globeMapped.json', true);
     xhr.onreadystatechange = function(e) {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
+                //Generate a random % of dates to split by(eg, if it's 7, then split globeMapped.json's data into 7 portions)
+                var numOfDates = Math.floor((Math.random() * 10) + 1);
+                for (i = 0; i < numOfDates; i++) {
+                  var newArray = [];
+                  datasetRandomized.push(newArray);
+                }
+
                 var data = JSON.parse(xhr.responseText);
                 window.data = data;
-                console.log(data);
+
+                //Iterates through the top-level of the json array, starts with date-data heirarchy
                 for (i=0;i<data.length;i++) {
-                    globe.addData(data[i][1], {format: 'magnitude', name: data[i][0], animated: true});
+                  var dataset = data[i][1];
+                  //Iterate through dataset through sets of 3, since each point depends on a 3-set index
+                  for (j = 0; j < dataset.length; j+=3) {
+                    var randomIndex = Math.floor(Math.random() * numOfDates);
+                    datasetRandomized[randomIndex].push(dataset[j]); //Latitude
+                    datasetRandomized[randomIndex].push(dataset[j + 1]); //Longitude
+                    datasetRandomized[randomIndex].push(dataset[j + 2]); //Magnitude
+                  }
                 }
-                globe.createPoints();
-                // setTime(globe,0)();
-                globe.animate();
+
+                window.onload = function() {
+                  var navTable = document.getElementById("navigationTable");
+                  var tableRow = document.createElement("tr");
+                  var dateRowHead = document.createElement("td");
+                  var dateRowHeadCell = document.createElement("span");
+
+                  dateRowHeadCell.onclick = function(){
+                    loadDateData(0);
+                  };
+                  dateRowHeadCell.innerHTML = "Dates";
+
+                  dateRowHead.appendChild(dateRowHeadCell);
+                  tableRow.appendChild(dateRowHead);
+
+                  var dateRowContents = document.createElement("td");
+                  for (i = 0; i < numOfDates; i++) {
+                    var dateBullet = createDateSpan(i);
+                    dateRowContents.appendChild(dateBullet);
+                  }
+                  tableRow.appendChild(dateRowContents);
+                  navTable.appendChild(tableRow);
+
+                  loadDateData(0);
+                };
             }
         }
     };
     xhr.send(null);
+  }
+
+  var loadDateData = function(date) {
+    console.log(date);
+    document.getElementById("date" + currentDate).style.color = '#555';
+    currentDate = date;
+    document.getElementById("date" + currentDate).style.color = '#fff';
+    document.getElementById('load').innerHTML = 'Loading...';
+    
+    var data = datasetRandomized[date];
+    window.data = data;
+    globe.clearData();
+    globe.addData(data);
+    globe.createPoints();
+    globe.animate();
+
+    document.getElementById('load').innerHTML = ' ';
+
+  };
+
+  var createDateSpan = function(dateValue) {
+    var spanTag = document.createElement("span");
+    spanTag.id = "date" + dateValue;
+    spanTag.className = "bull";
+    spanTag.innerHTML = "&#x25cf;"
+    spanTag.onclick = function(){
+      loadDateData(dateValue);
+    }
+    return spanTag;
+  }
+    
+  initData();
 }
--- globe-viz/static/js/globe.js
+++ globe-viz/static/js/globe.js
@@ -17,14 +17,18 @@ DAT.Globe = function(container, colorFn) {
 
   colorFn = colorFn || function(x) {
     var c = new THREE.Color();
-    c.setHSL( ( 0.6 - ( x * 0.5 ) ), 1.0, 0.5 );
+    if (x==0.0) {
+      c.setHSV( ( 0.6 - ( x * 0.5 ) ), 0, 0 );
+    } else {
+      c.setHSV( ( 0.6 - ( x * 0.5 ) ), 1.0, 1.0 );
+    }
     return c;
   };
 
   var Shaders = {
     'earth' : {
       uniforms: {
-        'texture': { type: 't', value: null }
+        'texture': { type: 't', value: 0, texture: null }
       },
       vertexShader: [
         'varying vec3 vNormal;',
@@ -60,14 +64,14 @@ DAT.Globe = function(container, colorFn) {
         'varying vec3 vNormal;',
         'void main() {',
           'float intensity = pow( 0.8 - dot( vNormal, vec3( 0, 0, 1.0 ) ), 12.0 );',
-          'gl_FragColor = vec4( 1.0, 1.0, 1.0, 0.5 ) * intensity;',
+          'gl_FragColor = vec4( 0.1, 0.2, 0.5, 0.7 ) * intensity;',
         '}'
       ].join('\n')
     }
   };
 
-  var camera, scene, renderer, w, h;
-  var mesh, atmosphere, point;
+  var camera, scene, sceneAtmosphere, renderer, w, h;
+  var vector, mesh, atmosphere, point;
 
   var overRenderer;
 
@@ -78,19 +82,18 @@ DAT.Globe = function(container, colorFn) {
 
   var mouse = { x: 0, y: 0 }, mouseOnDown = { x: 0, y: 0 };
   var rotation = { x: 0, y: 0 },
-      target = { x: Math.PI*1.7, y: Math.PI / 5.0 },
+      target = { x: Math.PI*3/2, y: Math.PI / 6.0 },
       targetOnDown = { x: 0, y: 0 };
+
 
   var distance = 100000, distanceTarget = 100000;
   var padding = 40;
   var PI_HALF = Math.PI / 2;
 
-  var ROTATIONSPEED = 0.001;
-  // var ROTATIONSPEED = 0.0;
-  var k = ROTATIONSPEED;
-  var f = false;
-  //RADIUS OF SPHERE 
-  var sphereRadius = 220;
+  /** Sphere Settings **/
+  var sphereRadius = 200;
+  var sphereWidthSegments = 40;
+  var sphereHeightSegments = 30;
 
   function init() {
 
@@ -101,20 +104,24 @@ DAT.Globe = function(container, colorFn) {
     w = container.offsetWidth || window.innerWidth;
     h = container.offsetHeight || window.innerHeight;
 
-
-    camera = new THREE.PerspectiveCamera(26, w / h, 1, 10000);
+    camera = new THREE.Camera(
+        30, w / h, 1, 10000);
     camera.position.z = distance;
 
-    scene = new THREE.Scene();
+    vector = new THREE.Vector3();
 
-    var geometry = new THREE.SphereGeometry(sphereRadius, 40, 30);
+    scene = new THREE.Scene();
+    sceneAtmosphere = new THREE.Scene();
+
+    var geometry = new THREE.Sphere(sphereRadius, sphereWidthSegments, sphereHeightSegments);
 
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir+'world.jpg');
+    uniforms['texture'].texture = THREE.ImageUtils.loadTexture(imgDir+'world' +
+        '.jpg');
 
-    material = new THREE.ShaderMaterial({
+    material = new THREE.MeshShaderMaterial({
 
           uniforms: uniforms,
           vertexShader: shader.vertexShader,
@@ -123,33 +130,42 @@ DAT.Globe = function(container, colorFn) {
         });
 
     mesh = new THREE.Mesh(geometry, material);
-    mesh.rotation.y = Math.PI;
-    scene.add(mesh);
+    mesh.matrixAutoUpdate = false;
+    scene.addObject(mesh);
 
     shader = Shaders['atmosphere'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    material = new THREE.ShaderMaterial({
+    material = new THREE.MeshShaderMaterial({
 
           uniforms: uniforms,
           vertexShader: shader.vertexShader,
-          fragmentShader: shader.fragmentShader,
-          side: THREE.BackSide,
-          blending: THREE.AdditiveBlending,
-          transparent: true
+          fragmentShader: shader.fragmentShader
 
         });
 
     mesh = new THREE.Mesh(geometry, material);
-    mesh.scale.set( 1.1, 1.1, 1.1 );
-    scene.add(mesh);
+    mesh.scale.x = mesh.scale.y = mesh.scale.z = 1.1;
+    mesh.flipSided = true;
+    mesh.matrixAutoUpdate = false;
+    mesh.updateMatrix();
+    sceneAtmosphere.addObject(mesh);
 
-    geometry = new THREE.CubeGeometry(0.75, 0.75, 1);
-    geometry.applyMatrix(new THREE.Matrix4().makeTranslation(0,0,-0.5));
+    geometry = new THREE.Cube(0.75, 0.75, 1, 1, 1, 1, null, false, { px: true,
+          nx: true, py: true, ny: true, pz: false, nz: true});
+
+    for (var i = 0; i < geometry.vertices.length; i++) {
+
+      var vertex = geometry.vertices[i];
+      vertex.position.z += 0.5;
+
+    }
 
     point = new THREE.Mesh(geometry);
 
     renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.autoClear = false;
+    renderer.setClearColorHex(0x000000, 0.0);
     renderer.setSize(w, h);
 
     renderer.domElement.style.position = 'absolute';
@@ -162,7 +178,7 @@ DAT.Globe = function(container, colorFn) {
 
     document.addEventListener('keydown', onDocumentKeyDown, false);
 
-    //window.addEventListener('resize', onWindowResize, false);
+    window.addEventListener('resize', onWindowResize, false);
 
     container.addEventListener('mouseover', function() {
       overRenderer = true;
@@ -174,88 +190,32 @@ DAT.Globe = function(container, colorFn) {
   }
 
   addData = function(data, opts) {
-    var lat, lng, size, color, i, step, colorFnWrapper;
+    var lat, lng, size, color, i;
 
-    opts.animated = opts.animated || false;
-    this.is_animated = opts.animated;
-    opts.format = opts.format || 'magnitude'; // other option is 'legend'
-    //console.log(opts.format);
-    if (opts.format === 'magnitude') {
-      step = 3;
-      colorFnWrapper = function(data, i) { return colorFn(data[i+2]); }
-    } else if (opts.format === 'legend') {
-      step = 4;
-      colorFnWrapper = function(data, i) { return colorFn(data[i+3]); }
-    } else {
-      throw('error: format not supported: '+opts.format);
-    }
-
-    if (opts.animated) {
-      if (this._baseGeometry === undefined) {
-        this._baseGeometry = new THREE.Geometry();
-        for (i = 0; i < data.length; i += step) {
-          lat = data[i];
-          lng = data[i + 1];
-//        size = data[i + 2];
-          color = colorFnWrapper(data,i);
-          size = 0;
-          addPoint(lat, lng, size, color, this._baseGeometry);
-        }
-      }
-      if(this._morphTargetId === undefined) {
-        this._morphTargetId = 0;
-      } else {
-        this._morphTargetId += 1;
-      }
-      opts.name = opts.name || 'morphTarget'+this._morphTargetId;
-    }
     var subgeo = new THREE.Geometry();
-    for (i = 0; i < data.length; i += step) {
+    for (i = 0; i < data.length; i += 3) {
       lat = data[i];
       lng = data[i + 1];
-      color = colorFnWrapper(data,i);
-      size = data[i + 2];
-      size = size*sphereRadius;
+      color = colorFn(data[i+2]);
+      size = 0; // data[i + 2]; // CHANGED
       addPoint(lat, lng, size, color, subgeo);
     }
-    if (opts.animated) {
-      this._baseGeometry.morphTargets.push({'name': opts.name, vertices: subgeo.vertices});
-    } else {
-      this._baseGeometry = subgeo;
-    }
+    this._baseGeometry = subgeo;
 
   };
 
   function createPoints() {
     if (this._baseGeometry !== undefined) {
-      if (this.is_animated === false) {
-        this.points = new THREE.Mesh(this._baseGeometry, new THREE.MeshBasicMaterial({
+      var points = new THREE.Mesh(this._baseGeometry, new THREE.MeshBasicMaterial({
               color: 0xffffff,
               vertexColors: THREE.FaceColors,
               morphTargets: false
-            }));
-      } else {
-        if (this._baseGeometry.morphTargets.length < 8) {
-          console.log('t l',this._baseGeometry.morphTargets.length);
-          var padding = 8-this._baseGeometry.morphTargets.length;
-          console.log('padding', padding);
-          for(var i=0; i<=padding; i++) {
-            console.log('padding',i);
-            this._baseGeometry.morphTargets.push({'name': 'morphPadding'+i, vertices: this._baseGeometry.vertices});
-          }
-        }
-        this.points = new THREE.Mesh(this._baseGeometry, new THREE.MeshBasicMaterial({
-              color: 0xffffff,
-              vertexColors: THREE.FaceColors,
-              morphTargets: true
-            }));
-      }
-      scene.add(this.points);
+      }));
+      scene.addObject(points);
     }
   }
 
   function addPoint(lat, lng, size, color, subgeo) {
-
     var phi = (90 - lat) * Math.PI / 180;
     var theta = (180 - lng) * Math.PI / 180;
 
@@ -265,29 +225,24 @@ DAT.Globe = function(container, colorFn) {
 
     point.lookAt(mesh.position);
 
-    point.scale.z = Math.max( size, 0.1 ); // avoid non-invertible matrix
+    point.scale.z = -size;
     point.updateMatrix();
-
-    for (var i = 0; i < point.geometry.faces.length; i++) {
+    var i;
+    for (i = 0; i < point.geometry.faces.length; i++) {
 
       point.geometry.faces[i].color = color;
 
     }
 
-    THREE.GeometryUtils.merge(subgeo, point);
+    GeometryUtils.merge(subgeo, point);
   }
 
   function onMouseDown(event) {
     event.preventDefault();
 
-    k = 0;
-    f = true;
-
     container.addEventListener('mousemove', onMouseMove, false);
     container.addEventListener('mouseup', onMouseUp, false);
     container.addEventListener('mouseout', onMouseOut, false);
-
-    target.y = rotation.y;
 
     mouseOnDown.x = - event.clientX;
     mouseOnDown.y = event.clientY;
@@ -312,10 +267,6 @@ DAT.Globe = function(container, colorFn) {
   }
 
   function onMouseUp(event) {
-
-    k = ROTATIONSPEED;
-    f = false;
-
     container.removeEventListener('mousemove', onMouseMove, false);
     container.removeEventListener('mouseup', onMouseUp, false);
     container.removeEventListener('mouseout', onMouseOut, false);
@@ -323,10 +274,6 @@ DAT.Globe = function(container, colorFn) {
   }
 
   function onMouseOut(event) {
-
-    k = ROTATIONSPEED;
-    f = false;
-
     container.removeEventListener('mousemove', onMouseMove, false);
     container.removeEventListener('mouseup', onMouseUp, false);
     container.removeEventListener('mouseout', onMouseOut, false);
@@ -343,11 +290,11 @@ DAT.Globe = function(container, colorFn) {
   function onDocumentKeyDown(event) {
     switch (event.keyCode) {
       case 38:
-        zoom(100);
+        zoom(zoomSpeed);
         event.preventDefault();
         break;
       case 40:
-        zoom(-100);
+        zoom(zoomSpeed * -1);
         event.preventDefault();
         break;
     }
@@ -361,81 +308,57 @@ DAT.Globe = function(container, colorFn) {
 
   function zoom(delta) {
     distanceTarget -= delta;
-    distanceTarget = distanceTarget > 1100 ? 1100 : distanceTarget;
+    distanceTarget = distanceTarget > 1000 ? 1000 : distanceTarget;
     distanceTarget = distanceTarget < 350 ? 350 : distanceTarget;
   }
 
   function animate() {
     requestAnimationFrame(animate);
     render();
-
   }
-
-
 
   function render() {
     zoom(curZoomSpeed);
 
-    target.x -= k;
-
-    rotation.x += (target.x - rotation.x) * 0.2;
-
-    if (f == true){
-      rotation.y += (target.y - rotation.y) * 0.2;}
-    if (f == false){
-      target.y = Math.PI / 5.0;
-      rotation.y += (target.y - rotation.y) * 0.02;
-    };
-
+    rotation.x += (target.x - rotation.x) * 0.1;
+    rotation.y += (target.y - rotation.y) * 0.1;
     distance += (distanceTarget - distance) * 0.3;
 
     camera.position.x = distance * Math.sin(rotation.x) * Math.cos(rotation.y);
     camera.position.y = distance * Math.sin(rotation.y);
     camera.position.z = distance * Math.cos(rotation.x) * Math.cos(rotation.y);
 
-    camera.lookAt(mesh.position);
+    vector.copy(camera.position);
 
+    renderer.clear();
     renderer.render(scene, camera);
-
+    renderer.render(sceneAtmosphere, camera);
   }
 
   init();
   this.animate = animate;
-
-
-  this.__defineGetter__('time', function() {
-    return this._time || 0;
-  });
-
-  this.__defineSetter__('time', function(t) {
-    var validMorphs = [];
-    var morphDict = this.points.morphTargetDictionary;
-    for(var k in morphDict) {
-      if(k.indexOf('morphPadding') < 0) {
-        validMorphs.push(morphDict[k]);
-      }
-    }
-    validMorphs.sort();
-    var l = validMorphs.length-1;
-    var scaledt = t*l+1;
-    var index = Math.floor(scaledt);
-    for (i=0;i<validMorphs.length;i++) {
-      this.points.morphTargetInfluences[validMorphs[i]] = 0;
-    }
-    var lastIndex = index - 1;
-    var leftover = scaledt - index;
-    if (lastIndex >= 0) {
-      this.points.morphTargetInfluences[lastIndex] = 1 - leftover;
-    }
-    this.points.morphTargetInfluences[index] = leftover;
-    this._time = t;
-  });
-
   this.addData = addData;
   this.createPoints = createPoints;
   this.renderer = renderer;
   this.scene = scene;
+  
+  this.clearData = function() {
+    // I am pretty sure I am doing it wrong, but there is no documentation I can
+    // find on the Web, and it is quarter past two in the night, so I will go with
+    // "this seems superficially to work enough for a throwaway website"
+
+    console.log(this.scene.__webglObjects);
+    console.log(this.scene.children);
+
+    if (this.scene.__webglObjects)
+      while (this.scene.__webglObjects.length > 1)
+        this.scene.__webglObjects.pop();
+    if (this.scene.children)
+      while (this.scene.children.length > 1)
+        this.scene.removeChild(this.scene.children[this.scene.children.length-1]);
+  };
 
   return this;
 
 };
+
